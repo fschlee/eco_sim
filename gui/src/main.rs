0e3de87e2eb9434a904155eb83065afa7220f7db
Feisal Schlee <fschlee@techfak.uni-bielefeld.de>
2019-11-24 19:18:32 +0100

    Handle all events before continuing with rendering/simulation

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -1,6 +1,6 @@
 #![feature(manually_drop_take)]
 
-use winit::{event_loop::EventLoop, window::{WindowBuilder, Window}};
+use winit::{event_loop::EventLoop, platform::desktop::EventLoopExtDesktop, window::{WindowBuilder, Window}};
 use log::{debug, error, info, trace, warn};
 use winit::dpi::LogicalSize;
 use std::time::Instant;
@@ -16,6 +16,7 @@ pub mod error;
 
 use renderer::init::{init_device, InstSurface, DeviceInit};
 use crate::error::Error;
+use winit::event::Event::EventsCleared;
 
 const MAX_RENDER_FAILS : u32 = 100;
 
@@ -102,7 +103,7 @@ fn main() {
     };
 }
 
-fn game_loop<IS: InstSurface + 'static>(event_loop: EventLoop<()>, window: Window, device_init : DeviceInit<IS>) -> Result<(), Error> {
+fn game_loop<IS: InstSurface + 'static>(mut event_loop: EventLoop<()>, window: Window, device_init : DeviceInit<IS>) {
 
     let window_client_area = window
         .inner_size()
@@ -115,52 +116,51 @@ fn game_loop<IS: InstSurface + 'static>(event_loop: EventLoop<()>, window: Windo
     let glyph_cache = conrod_core::text::GlyphCache::builder().dimensions(1024,1024).build();
     let mut ui_processor = renderer::con_back::UiProcessor::from_glyph_cache_with_filled_queue(glyph_cache);
     let mut ui_cmds = Vec::new();
+
     event_loop.run( move|event, window_target, cntr_flow| {
-        let close = ui_state.process(event, &game_state);
-        if close || fail_counter > MAX_RENDER_FAILS {
-            *cntr_flow = winit::event_loop::ControlFlow::Exit;
+        if event != EventsCleared {
+            let close = ui_state.process(event, &game_state);
+            if close || fail_counter > MAX_RENDER_FAILS {
+                *cntr_flow = winit::event_loop::ControlFlow::Exit;
+            }
         }
-        let now = Instant::now();
-        let delta = now.duration_since(instant).as_secs_f32();
-        if delta < MIN_FRAME_DELAY {
-            return;
-        }
-        instant = now;
-        ui_state.update(&game_state);
-        if let Some(mut prims) = ui_state.conrod.draw_if_changed(){
-            let dpi_factor = ui_state.window.hidpi_factor() as f32;
-            let winit::dpi::LogicalSize{width, height, ..} = ui_state.window.inner_size();
-            let (cmds, vtx) = ui_processor.process_primitives(& mut prims, dpi_factor, width as f32, height as f32)
-                .or_else(|_| {
-                    ui_processor.update_gyph_cache(renderer::con_back::GlyphWalker::new(ui_state.conrod.draw(), dpi_factor ));
-                    ui_processor.process_primitives(& mut ui_state.conrod.draw(), dpi_factor, width as f32, height as f32)
-                }).unwrap();
-            renderer.set_ui_buffer(vtx).unwrap();
-            ui_cmds = cmds;
-            if ui_processor.tex_updated {
-                let id0 = renderer::memory::Id::new(0);
-                let spec = ui_processor.get_texture_spec();
-                if renderer.texture_manager.texture_count() < 1 {
-                    let id = renderer.add_texture(&spec);
-                    assert_eq!(id, Ok(id0));
-                } else {
-                    if let Err(_) = renderer.replace_texture(id0, &spec) {
+        else {
+            ui_state.update(&game_state);
+            if let Some(mut prims) = ui_state.conrod.draw_if_changed(){
+                let dpi_factor = ui_state.window.hidpi_factor() as f32;
+                let winit::dpi::LogicalSize{width, height, ..} = ui_state.window.inner_size();
+                let (cmds, vtx) = ui_processor.process_primitives(& mut prims, dpi_factor, width as f32, height as f32)
+                    .or_else(|_| {
+                        ui_processor.update_gyph_cache(renderer::con_back::GlyphWalker::new(ui_state.conrod.draw(), dpi_factor ));
+                        ui_processor.process_primitives(& mut ui_state.conrod.draw(), dpi_factor, width as f32, height as f32)
+                    }).unwrap();
+                renderer.set_ui_buffer(vtx).unwrap();
+                ui_cmds = cmds;
+                if ui_processor.tex_updated {
+                    let id0 = renderer::memory::Id::new(0);
+                    let spec = ui_processor.get_texture_spec();
+                    if renderer.texture_manager.texture_count() < 1 {
                         let id = renderer.add_texture(&spec);
                         assert_eq!(id, Ok(id0));
+                    } else {
+                        if let Err(_) = renderer.replace_texture(id0, &spec) {
+                            let id = renderer.add_texture(&spec);
+                            assert_eq!(id, Ok(id0));
+                        }
                     }
                 }
             }
-        }
-        let now = Instant::now();
-        let delta = now.duration_since(instant).as_secs_f32();
-        instant = now;
-        game_state.update(ui_state.actions.drain(..), delta);
-        let sim_updates = game_state.get_render_data();
-        match renderer.tick(&ui_cmds, ui_state.ui_updates.drain(..), &sim_updates) {
-            Ok(_) => fail_counter = 0,
-            Err(err) => {
-                error!("{}", err);
-                fail_counter += 1;
+            let now = Instant::now();
+            let delta = now.duration_since(instant).as_secs_f32();
+            instant = now;
+            game_state.update(ui_state.actions.drain(..), delta);
+            let sim_updates = game_state.get_render_data();
+            match renderer.tick(&ui_cmds, ui_state.ui_updates.drain(..), &sim_updates) {
+                Ok(_) => fail_counter = 0,
+                Err(err) => {
+                    error!("{}", err);
+                    fail_counter += 1;
+                }
             }
         }
     });
